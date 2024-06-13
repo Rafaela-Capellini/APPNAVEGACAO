209efda48b2086cab92d2bd6d77fee68e3414970
Rafaela.Capellini <Rafaelaegabriela1@gmail.com>
2024-06-13 13:43:38 -0300

    Adicionamos uma listagem de produtos com flatiList

---
+++ Src/data/dados.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        id: "1",
+        nome: "Ipad",
+        preco: "789,00",
+        fabricante: "Apple",
+        cor: "Branco",
+        linkImagem: "https://cdn.pixabay.com/photo/2014/11/12/15/48/apple-528461_1280.jpg"
+    },
+    {
+        id: "2",
+        nome: "Iphone",
+        preco: "2.339,00",
+        fabricante: "Apple",
+        cor: "Branco",
+        linkImagem: "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        id: "3",
+        nome: "MacBook",
+        preco: "12.654,00",
+        fabricante: "Apple",
+        cor: "Branco",
+        linkImagem: "https://images.pexels.com/photos/434346/pexels-photo-434346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
+]
--- Src/views/ProdutosScreen.js
+++ Src/views/ProdutosScreen.js
@@ -1,22 +1,45 @@
-import {View, Text, Button} from "react-native";
+import { View, Text, Button, FlatList, StyleSheet } from "react-native";
+
+import produtos from "../data/dados";
+import { Divider, List } from "react-native-paper";
+
 
 export default function ProdutosScreen(props) {
+  function getProdutoItem({ item: prod }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Produtos Screen</Text>
-        <Button
-          title='ir para Home'
-          onPress={() => props.navigation.navigate('Home')}
+      <View>
+        <List.Item style={estilos.listItem}
+          title={prod.nome}
+          description={prod.preco}
+          left={() => <List.Image variant="image"
+            source={{ uri: prod.linkImagem }}
+          />}
         />
-        <Button
-          title='ir para Sobre'
-          onPress={() => props.navigation.navigate('Sobre')}
-        />
-          <Button
-          title='ir para Contato'
-          onPress={() => props.navigation.navigate('Contato')}
-        />
+        <Divider />
       </View>
-    );
+    )
+
   }
-  
+  return (
+    <View style={estilos.container}>
+      <FlatList
+        data={produtos}       //data indica onde os dados estão vindo
+        keyExtractor={prod => prod.id}   //ele espera uma função (usar o produto id)
+        renderItem={getProdutoItem}        //renderiza o componente
+        style={estilos.flatList}
+      />
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  flatList: {
+    width: "95%"
+  }
+})
